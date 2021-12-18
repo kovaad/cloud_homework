c2eff8dad9e0bebd2e871e986b45e06989be859b
kovaad <kovacs.adam@krtk.hu>
2021-12-19 00:12:49 +0100

    add script

---
+++ bronze_views.sql
@@ -0,0 +1,9 @@
+CREATE EXTERNAL TABLE
+kovaad_homework.bronze_views (
+    article STRING,
+    views INT,
+    rank INT,
+    date DATE,
+    retrieved_at TIMESTAMP) 
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://kerekerdo/datalake/views/';
